87709d933012d5948ef71fe24f74eb3ff445a1cd
AlejandroDuron <tato.duron@gmail.com>
2026-09-19 09:06:17 -0600

    Parte A y B completa

--- observability/variables.tf
+++ observability/variables.tf
@@ -2,4 +2,9 @@ variable "iam_user_name" {
   description = "Nombre del usuario IAM creado en el Laboratorio 3" 
   type        = string 
   default     = "devsecops-lab-user" 
+}
+
+variable "alert_email" { 
+  description = "Correo que recibirá las alertas de CloudWatch" 
+  type        = string 
 } 
